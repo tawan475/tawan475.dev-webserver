7be60ea2318bf9eecf1c685f6503d3b832233c81
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-04-07 15:05:16 +0700

    add cloudflareIpProcessor

---
+++ libs/cloudflareIpProcessor.js
@@ -0,0 +1,29 @@
+const ipaddr = require("ipaddr.js");
+const cloudflareIP = require('@tawan475/cloudflareip');
+const cfIP = new cloudflareIP();
+
+module.exports = ((req, res, next) => {
+    if (req.headers['cf-connecting-ip'] && cfIP.isCloudflareIP(req)) {
+        let realIP = ipaddr.process(req.headers['cf-connecting-ip']);
+        let cloudflareip = ipaddr.process(req.socket.remoteAddress);
+
+        req.headers['from-cloudflare'] = true;
+        req.headers['cloudflare-ip'] = cloudflareip;
+
+        req.cloudflareip = cloudflareip.toString();
+        req.cloudflareipver = cloudflareip.kind();
+
+        req.trustedip = realIP.toString();
+        req.trustedipver = realIP.kind();
+
+        req.cloudflare = true;
+    }
+
+    if (!req.cloudflare) {
+        let ip = ipaddr.process(req.socket.remoteAddress);
+        req.trustedip = ip.toString();
+        req.trustedipver = ip.kind();
+    }
+
+    next();
+})
