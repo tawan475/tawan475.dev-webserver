638f953b5f6a2919d4fafbc5103ffd4b2188a8ae
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-05-02 06:34:30 +0700

    add stuff

--- tawan475.dev.js
+++ tawan475.dev.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const fs = require('fs');
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -30,4 +32,20 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500).json({ status: err.status || 500, message: err.message || 'Internal Server Error' });
 });
 
-module.exports = app;
+if (process.env.NODE_ENV !== 'production') process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+const options = process.env.NODE_ENV === "production" ? {
+    key: fs.readFileSync('./libs/ssl/private.key.pem'),
+    cert: fs.readFileSync('./libs/ssl/domain.cert.pem'),
+    ca: fs.readFileSync('./libs/ssl/intermediate.cert.pem')
+} : {
+    key: fs.readFileSync('./libs/ssl/localhost/localhost.key'),
+    cert: fs.readFileSync('./libs/ssl/localhost/localhost.crt')
+}
+
+require('http').createServer(app).listen(process.env.PORT, () => {
+    console.log(`listening at HTTP`)
+});
+
+require('https').createServer(options, app).listen(process.env.SECURE_PORT, () => {
+    console.log(`listening at HTTPS`)
+});
