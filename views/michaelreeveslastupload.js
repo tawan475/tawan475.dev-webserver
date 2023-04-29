fcc4227d0d61f3d70678a97eed91abf41e1af08d
tawan475 <43491902+tawan475@users.noreply.github.com>
2023-04-30 01:19:02 +0700

    update michael reeves last upload

--- views/michaelreeveslastupload.js
+++ views/michaelreeveslastupload.js
@@ -1,5 +1,15 @@
+const ytpl = require('ytpl');
+const ytdl = require('ytdl-core');
+
 module.exports = {
-	GET: (req, res, next, pathInfo, locals) => {
+	GET: async (req, res, next, pathInfo, locals) => {
+        let channel = await ytpl('UCtHaxi4GTYDpJgMSGy7AeSw');
+        let lastVideoId = channel.items[0].id;
+        let lastVideo = await ytdl.getInfo(lastVideoId); 
+        let lastUploadTime = new Date(lastVideo.videoDetails.publishDate).getTime();
+        
+        Object.assign(locals, { lastUpload: lastUploadTime, lastVideoId: lastVideoId })
+
 		return res.renderMin(pathInfo.viewPath, locals, function(err, html) {
 			if (err) return next(err);
 			return res.send(html);
